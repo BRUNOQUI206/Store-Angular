f6023acdf66998f24d1c3a6f9da98e91e78d25c6
Bruno <brunoguilher1@gmail.com>
2024-02-23 14:18:53 -0300

    Add boxicons in HTML

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,5 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    {
-        path: "Home",
-        component: HomeComponent
-    }
+    {path: "", component: HomeComponent}
 ];
