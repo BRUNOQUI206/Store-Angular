1021a9cbd618917b233ddff3f39aac5e37ba4c79
Bruno <brunoguilher1@gmail.com>
2024-02-24 15:52:53 -0300

    Create star carousel

---
+++ src/app/components/home/carousel/carousel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css'
+})
+export class CarouselComponent {
+
+}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     HeaderComponent,
+    CarouselComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
