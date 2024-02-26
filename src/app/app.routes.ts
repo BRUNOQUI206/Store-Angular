240bd32a3b7bfa3b3c44ddf77815af74087590ad
Bruno <brunoguilher1@gmail.com>
2024-02-26 17:22:44 -0300

    Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent}
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+export class AppRoutingModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
 import { CarouselComponent } from './carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    HeaderComponent,
     CarouselComponent
   ],
   templateUrl: './home.component.html',
